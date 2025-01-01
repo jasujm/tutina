607714d448f44e964a9360f53870152b69ccfdcb
Jaakko Moisio <jaakko@moisio.fi>
2025-01-01 16:29:16 +0200

    add documentation to openapi

--- tutina-app/tutina/app/routers/predictions.py
+++ tutina-app/tutina/app/routers/predictions.py
@@ -15,39 +15,83 @@ router = fastapi.APIRouter(
     tags=["predictions"],
 )
 
-FeatureTimeSeries = dict[datetime, float]
+
+class FeatureTimeSeries(pydantic.RootModel):
+    """Time series of numeric data"""
+
+    root: dict[datetime, float]
+
+
+class FeaturesByName(pydantic.RootModel):
+    """Collection of feature time series by name"""
+
+    root: dict[str, FeatureTimeSeries]
+
+
+class Forecasts(pydantic.BaseModel):
+    """Weather forecast as time series"""
+
+    temperature: FeatureTimeSeries
+
 
 SVG_MEDIA_TYPE = "image/svg+xml"
 
 
-class Forecasts(TypedDict):
-    temperature: FeatureTimeSeries
-
-
 class TutinaModelInput(pydantic.BaseModel):
-    """Input to the tutina model"""
+    """Serialized input to the tutina model"""
 
     history: Annotated[
-        dict[str, FeatureTimeSeries],
+        FeaturesByName,
         pydantic.Field(
-            description="Measurement history prior to the predicted timesteps"
+            description="History of measurements prior to the predicted timesteps",
+            example={
+                "temperature_bedroom": {
+                    "2020-01-01T07:00:00Z": 20.0,
+                    "2020-01-01T08:00:00Z": 21.0,
+                },
+                "temperature_outdoor": {
+                    "2020-01-01T07:00:00Z": -5.0,
+                    "2020-01-01T08:00:00Z": -4.0,
+                },
+            },
         ),
     ]
     control: Annotated[
-        dict[str, FeatureTimeSeries],
-        pydantic.Field(description="Control input for the predicted timesteps"),
+        FeaturesByName,
+        pydantic.Field(
+            description="Control input for the predicted timesteps",
+            example={
+                "hvac_state_heat_radiator": {
+                    "2020-01-01T09:00:00Z": 1.0,
+                    "2020-01-01T10:00:00Z": 1.0,
+                },
+                "hvac_temperature_heat_radiator": {
+                    "2020-01-01T09:00:00Z": 21.0,
+                    "2020-01-01T10:00:00Z": 21.0,
+                },
+            },
+        ),
     ]
     forecasts: Annotated[
         Forecasts,
-        pydantic.Field(description="Wather forecast for the predicted timesteps"),
+        pydantic.Field(
+            description="Weather forecast for the predicted timesteps",
+            example={
+                "temperature": {
+                    "2020-01-01T08:00:00Z": -4.5,
+                    "2020-01-01T09:00:00Z": -3.5,
+                    "2020-01-01T10:00:00Z": -3.5,
+                }
+            },
+        ),
     ]
 
 
 def _request_body_to_df(model_input: TutinaModelInput):
     return TutinaInputFeatures(
-        history=pd.DataFrame.from_dict(model_input.history),
-        control=pd.DataFrame.from_dict(model_input.control),
-        forecasts=pd.DataFrame.from_dict(model_input.forecasts),  # type: ignore
+        history=pd.DataFrame.from_dict(model_input.history.model_dump()),
+        control=pd.DataFrame.from_dict(model_input.control.model_dump()),
+        forecasts=pd.DataFrame.from_dict(model_input.forecasts.model_dump()),
     )
 
 
@@ -67,7 +111,31 @@ def _create_plot_response(
     return fresponses.StreamingResponse(f, media_type=SVG_MEDIA_TYPE)
 
 
-@router.post("", summary="Create new prediction")
+@router.post(
+    "",
+    summary="Create new prediction",
+    responses={
+        200: {
+            "content": {
+                "application/json": {
+                    "schema": {
+                        "example": {
+                            "temperature_bedroom": {
+                                "2020-01-01T09:00:00Z": 21.0,
+                                "2020-01-01T10:00:00Z": 20.5,
+                            },
+                            "temperature_outdoor": {
+                                "2020-01-01T09:00:00Z": -5.0,
+                                "2020-01-01T10:00:00Z": -4.0,
+                            },
+                        },
+                    }
+                },
+                SVG_MEDIA_TYPE: {},
+            }
+        }
+    },
+)
 def post_predictions(
     tutina_model: Annotated[TutinaModelWrapper, fastapi.Depends(get_tutina_model)],
     model_input: TutinaModelInput,
@@ -77,7 +145,7 @@ def post_predictions(
             description="By default, return the response in `application/json`. Request `image/*` or `image/svg+xml` for a plot in SVG format."
         ),
     ] = None,
-) -> dict[str, FeatureTimeSeries]:
+) -> FeaturesByName:
     accepted_media_types = _parse_accepted_media_types(accept)
     model_input_dfs = _request_body_to_df(model_input)
     prediction = tutina_model.predict_single(model_input_dfs)
