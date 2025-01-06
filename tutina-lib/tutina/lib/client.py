e720bfabe7a1ce302351d6721fe916984c8c7bcd
Jaakko Moisio <jaakko@moisio.fi>
2025-01-06 08:57:23 +0200

    use orjson in tutina client

--- tutina-lib/tutina/lib/client.py
+++ tutina-lib/tutina/lib/client.py
@@ -1,8 +1,8 @@
 import contextlib
 import enum
 import functools
-import json
 from datetime import datetime, timedelta, timezone
+import orjson
 from typing import Iterable
 
 import aiohttp
@@ -11,17 +11,6 @@ import jwt
 from .types import Forecast, Hvac, Measurement, OpeningState
 
 EXP_TIME_IN_MINUTES = 5
-
-
-def _json_default(o):
-    if isinstance(o, datetime):
-        return o.isoformat()
-    if isinstance(o, enum.Enum):
-        return o.value
-    raise TypeError(f"Cannot serialize {o!r}")
-
-
-_json_serialize = functools.partial(json.dumps, default=_json_default)
 
 
 def _get_exp():
@@ -37,35 +26,38 @@ class TutinaClient:
         self._token_secret = token_secret
 
     async def submit_measurements(self, measurements: Iterable[Measurement]):
-        serialized_data = [m.model_dump() for m in measurements]
+        serialized_data = orjson.dumps([m.model_dump() for m in measurements])
         async with self._session.post(
-            "/data/measurements", json=serialized_data, headers=self._get_headers()
+            "/data/measurements", data=serialized_data, headers=self._get_headers()
         ):
             pass
 
     async def submit_hvacs(self, hvacs: Iterable[Hvac]):
-        serialized_data = [h.model_dump() for h in hvacs]
+        serialized_data = orjson.dumps([h.model_dump() for h in hvacs])
         async with self._session.post(
-            "/data/hvacs", json=serialized_data, headers=self._get_headers()
+            "/data/hvacs", data=serialized_data, headers=self._get_headers()
         ):
             pass
 
     async def submit_opening_states(self, opening_states: Iterable[OpeningState]):
-        serialized_data = [o.model_dump() for o in opening_states]
+        serialized_data = orjson.dumps([o.model_dump() for o in opening_states])
         async with self._session.post(
-            "/data/opening_states", json=serialized_data, headers=self._get_headers()
+            "/data/opening_states", data=serialized_data, headers=self._get_headers()
         ):
             pass
 
     async def submit_forecasts(self, forecasts: Iterable[Forecast]):
-        serialized_data = [f.model_dump() for f in forecasts]
+        serialized_data = orjson.dumps([f.model_dump() for f in forecasts])
         async with self._session.post(
-            "/data/forecasts", json=serialized_data, headers=self._get_headers()
+            "/data/forecasts", data=serialized_data, headers=self._get_headers()
         ):
             pass
 
     def _get_headers(self):
-        return {"Authorization": f"Bearer {self._generate_token()}"}
+        return {
+            "Authorization": f"Bearer {self._generate_token()}",
+            "Content-Type": "application/json"
+        }
 
     def _generate_token(self):
         return jwt.encode({"exp": _get_exp()}, self._token_secret)
@@ -74,6 +66,6 @@ class TutinaClient:
 @contextlib.asynccontextmanager
 async def create_client(base_url: str, token_secret: str):
     async with aiohttp.ClientSession(
-        base_url=base_url, raise_for_status=True, json_serialize=_json_serialize
+        base_url=base_url, raise_for_status=True
     ) as session:
         yield TutinaClient(session, token_secret)
