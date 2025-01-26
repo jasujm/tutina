2d42b9768ca2346a9d362a1d38b2df99af3db7b1
Jaakko Moisio <jaakko@moisio.fi>
2025-01-26 21:12:04 +0200

    configure ha addon logging

--- tutina-ha/run.sh
+++ tutina-ha/run.sh
@@ -6,5 +6,31 @@ export tutina_owm__api_key=$(bashio::config "owm_api_key")
 export tutina_owm__coordinates=$(bashio::config "owm_coordinates")
 export tutina_tutina__base_url=$(bashio::config "tutina_base_url")
 export tutina_tutina__token_secret=$(bashio::config "tutina_token_secret")
+export tutina_logging='{
+  "version": 1,
+  "loggers": {
+    "": {
+      "handlers": ["default"],
+      "level": "WARNING"
+    },
+    "tutina": {
+      "handlers": ["default"],
+      "level": "INFO",
+      "propagate": false
+    }
+  },
+  "handlers": {
+    "default": {
+      "level": "INFO",
+      "class": "logging.StreamHandler",
+      "formatter": "default"
+    }
+  },
+  "formatters": {
+    "default": {
+      "format": "[%(asctime)s] %(levelname)-8s: %(name)s %(message)s"
+    }
+  }
+}'
 
 /usr/src/tutina/tutina-ha/.venv/bin/tutina ha
